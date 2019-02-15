72b2fc11956958bbf67eef7aa520451061e9f33a
Przemek <vatier@gazeta.pl>
2019-02-15 23:47:07 +0100

    set interval to 10 minutes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const persister = require("./persister");
 const tools = require("./tools");
 const config = require("./config");
 
-const INTERVAL_MINUTES = 1;
+const INTERVAL_MINUTES = 10;
 
 tools.logToConsole("Setting interval time to minutes: " + INTERVAL_MINUTES);
 
